b905def0f26f2c93fe1ebe8196627558351f14d0
Michael Montgomery <michaelmontgomery@HW-GPI-F0Q6WQJ7.local>
2026-05-12 12:58:52 -0400

    modifiede .env

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ import './header.css';
 function Header(props) {
     return (
         <header className="header">
-            <h1 style={{color: `${props.city['primary color']}`}}>Notifi</h1>
+            <h1 style={{color: `${props.city['primary color']} || 'black'`}}>Notifi</h1>
         </header>
     );
 }
