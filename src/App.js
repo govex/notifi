82321d764ad1f89bba34f193c31fbd8ceada388c
Michael Montgomery <michaelmontgomery@MW-GPI-F0Q6WQJ7.local>
2026-05-19 08:51:50 -0400

    switched from browserrouter to hashrouter for gh-pages compatibility.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import UserSignup from './pages/user-signup/user-signup';
 import CitySignup from './pages/city-signup/city-signup';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/go/:code' element={<UserSignup />} />
       <Route path='/city-signup' element={<CitySignup></CitySignup>} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
